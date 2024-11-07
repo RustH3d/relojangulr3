89a64c52f798b80b00ce10421457f8284a20e70c
RustH3d <humbertoa00327@gmail.com>
2024-11-07 14:11:18 -0400

    4torelojagrgado

--- src/app/clock/clock.component.ts
+++ src/app/clock/clock.component.ts
@@ -12,9 +12,10 @@ import { FormsModule } from '@angular/forms';
 })
 
 export class ClockComponent implements OnInit, OnDestroy {
-  showDigital = true;
+  showDigital = false;
   showAnalog = false;
   showBinary = false;
+  showCustom = false;
   private intervalId: any;
   userTime: Date = new Date();
   isEditing = false;
@@ -48,15 +49,14 @@ export class ClockComponent implements OnInit, OnDestroy {
     this.showDigital = type === 'digital';
     this.showAnalog = type === 'analog';
     this.showBinary = type === 'binary';
+    this.showCustom = type === 'custom';
   }
 
-  // Función para habilitar el modo de edición
   enableEditing() {
     this.isEditing = true;
     this.stopAutoUpdate();  // Detener la actualización automática cuando estamos editando
   }
 
-  // Función para aplicar los cambios de la hora
   applyTimeChanges() {
     this.userTime.setHours(this.hourInput);
     this.userTime.setMinutes(this.minuteInput);
@@ -99,5 +99,20 @@ export class ClockComponent implements OnInit, OnDestroy {
   get secondsBinary(): number[][] {
     const seconds = this.userTime.getSeconds();
     return [this.toBinaryArray(Math.floor(seconds / 10), 3), this.toBinaryArray(seconds % 10, 4)];
+  }
+
+  // Funciones para manejar el fondo y los íconos del reloj personalizado
+  get customClockBackground(): string {
+    const hour = this.userTime.getHours();
+    if (hour >= 6 && hour < 18) {
+      return 'linear-gradient(to right, #FF7E5F, #FEB47B)'; // Colores cálidos (mañana/día)
+    } else {
+      return 'linear-gradient(to right, #2C3E50, #34495E)'; // Colores fríos (noche)
+    }
+  }
+
+  get sunOrMoonIcon(): string {
+    const hour = this.userTime.getHours();
+    return hour >= 6 && hour < 18 ? '☀️' : '🌙'; // Sol durante el día, luna durante la noche
   }
 }
