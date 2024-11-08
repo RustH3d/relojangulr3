a6e611f5f83042ab178f64dfe9b9b60f9d7fbf84
RustH3d <humbertoa00327@gmail.com>
2024-11-08 00:52:36 -0400

    ESTA BELLOOOOOOOOO

--- src/app/reloj-marcador/reloj-marcador.component.ts
+++ src/app/reloj-marcador/reloj-marcador.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+
 @Component({
   selector: 'app-reloj-marcador',
   standalone: true,
