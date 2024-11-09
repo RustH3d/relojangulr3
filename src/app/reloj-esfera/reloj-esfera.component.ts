ff3bdda034ae93a35b2acdea8cacc18ec07f0ad4
RustH3d <humbertoa00327@gmail.com>
2024-11-08 23:02:36 -0400

    Reloj de esferas completo

--- src/app/reloj-esfera/reloj-esfera.component.ts
+++ src/app/reloj-esfera/reloj-esfera.component.ts
@@ -11,12 +11,15 @@ import { FormsModule } from '@angular/forms';;
 })
 export class RelojEsferaComponent implements OnInit, OnDestroy{
 
-  horas: number= 0;
+  horas: number = 0;
   minutos: number = 0;
   segundos: number = 0;
+  inputHoras: number = 0;
+  inputMinutos: number = 0;
+  inputSegundos: number = 0;
   private interval: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.iniciarReloj();
@@ -27,11 +30,43 @@ export class RelojEsferaComponent implements OnInit, OnDestroy{
   }
 
   iniciarReloj() {
+    clearInterval(this.interval);
     this.interval = setInterval(() => {
       const now = new Date();
       this.horas = now.getHours();
       this.minutos = now.getMinutes();
       this.segundos = now.getSeconds();
+    }, 1000);
+  }
+
+  detenerReloj() {
+    clearInterval(this.interval);
+  }
+
+  aplicarCambios() {
+    // Asignamos los valores de los inputs al reloj y detenemos la actualización automática
+    this.horas = this.inputHoras;
+    this.minutos = this.inputMinutos;
+    this.segundos = this.inputSegundos;
+    this.detenerReloj();
+    this.iniciarRelojManual();
+  }
+
+  iniciarRelojManual() {
+    clearInterval(this.interval);
+    this.interval = setInterval(() => {
+      this.segundos++;
+      if (this.segundos >= 60) {
+        this.segundos = 0;
+        this.minutos++;
+      }
+      if (this.minutos >= 60) {
+        this.minutos = 0;
+        this.horas++;
+      }
+      if (this.horas >= 24) {
+        this.horas = 0;
+      }
     }, 1000);
   }
 }
