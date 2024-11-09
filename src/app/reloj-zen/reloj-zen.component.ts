f002ab7a1bc1882116b67434e8f0c4e07e6868fa
RustH3d <humbertoa00327@gmail.com>
2024-11-09 02:58:20 -0400

    mejorando

--- src/app/reloj-zen/reloj-zen.component.ts
+++ src/app/reloj-zen/reloj-zen.component.ts
@@ -16,6 +16,7 @@ export class RelojZenComponent implements OnInit{
   progresoHoras: number = 0;
   progresoMinutos: number = 0;
   progresoSegundos: number = 0;
+  horaDigital: string = '';
 
   ngOnInit(): void {
     setInterval(() => {
@@ -23,10 +24,21 @@ export class RelojZenComponent implements OnInit{
       this.horas = currentTime.getHours();
       this.minutos = currentTime.getMinutes();
       this.segundos = currentTime.getSeconds();
-      
+
+      // Formato de 24 horas para el reloj digital
+      this.horaDigital = this.formatTime(this.horas, this.minutos, this.segundos);
+
       this.progresoHoras = (this.horas % 12) / 12 * 100; // De 0 a 100%
       this.progresoMinutos = this.minutos / 60 * 100;  // De 0 a 100%
       this.progresoSegundos = this.segundos / 60 * 100;  // De 0 a 100%
     }, 1000);  // Actualizar cada segundo
+  }
+
+  // Método para dar formato a la hora en formato HH:mm:ss
+  formatTime(hours: number, minutes: number, seconds: number): string {
+    const hourString = hours < 10 ? `0${hours}` : `${hours}`;
+    const minuteString = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    const secondString = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    return `${hourString}:${minuteString}:${secondString}`;
   }
 }
